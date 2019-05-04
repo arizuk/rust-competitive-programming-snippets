85f91ab20b2b0ce443efb6f38ccd0eb573d9c595
masaki.arizuka <masaki.arizuka@gmail.com>
2019-05-04 11:23:13 +0900

    Add a script to update vscode config

---
+++ vscode_install.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+set -eu
+mkdir -p tmp
+cargo snippet -t vscode > tmp/cargo_snippets.json
+jq -s -M add vscode.json tmp/cargo_snippets.json > tmp/rust.json
+cp tmp/rust.json ${VSCODE_SNIPPET_PATH}/rust.json
