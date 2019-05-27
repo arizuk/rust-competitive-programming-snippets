2e0379c9b77dbfa080eba902bf06a23df403eddf
masaki.arizuka <masaki.arizuka@gmail.com>
2019-05-27 23:37:16 +0900

    Add binary_search

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,3 +1,23 @@
+#[snippet = "binary_search"]
+pub fn binary_search<T: Ord>(a: &Vec<T>, x: &T) -> Result<usize, usize> {
+    use std::cmp::Ordering;
+    let mut l = 0;
+    let mut r = a.len();
+
+    while l != r {
+        let m = l + (r - l) / 2;
+        match a[m].cmp(x) {
+            Ordering::Less => l = m + 1,
+            Ordering::Equal | Ordering::Greater => r = m,
+        }
+    }
+    if l < a.len() && a[l].cmp(x) == Ordering::Equal {
+        Ok(l)
+    } else {
+        Err(r)
+    }
+}
+
 #[snippet = "lower_bound"]
 pub fn lower_bound<T: Ord>(a: &Vec<T>, x: &T) -> Option<usize> {
     use std::cmp::Ordering;
@@ -41,6 +61,9 @@ pub fn uppper_bound<T: Ord>(a: &Vec<T>, x: &T) -> Option<usize> {
 #[test]
 fn test_binary_search() {
     let a = vec![1, 2, 4, 4, 7, 12, 54, 60];
+
+    assert_eq!(binary_search(&a, &4), Ok(2));
+    assert_eq!(binary_search(&a, &5), Err(4));
 
     assert_eq!(lower_bound(&a, &0), Some(0));
     assert_eq!(lower_bound(&a, &4), Some(2));
