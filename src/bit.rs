fb704161aba3a8c3fb84554aaa19eef19dfa8a41
masaki.arizuka <masaki.arizuka@gmail.com>
2019-05-29 00:43:25 +0900

    Use generics

--- src/bit.rs
+++ src/bit.rs
@@ -1,40 +1,49 @@
 #[snippet = "binary_indexed_tree"]
-#[derive(Debug)]
-pub struct BIT {
-    bits: Vec<u64>,
-}
+pub mod bit {
+    use std::ops::{AddAssign, Sub};
 
-#[snippet = "binary_indexed_tree"]
-impl BIT {
-    pub fn new(size: usize) -> Self {
-        let mut bit_size = 1;
-        while bit_size <= size {
-            bit_size *= 2;
-        }
-        BIT {
-            bits: vec![0; bit_size + 1],
-        }
+    #[derive(Debug)]
+    pub struct BIT<T> {
+        bits: Vec<T>,
+        init: T
     }
 
-    // i: 1-indexed but returns 0 if i=0 is given.
-    pub fn sum(&mut self, i: usize) -> u64 {
-        let mut i = i as i64;
-        let mut ret = 0;
-        while i > 0 {
-            ret += self.bits[i as usize];
-            i -= i & -i; // Add a last bit with 1
+    impl <T> BIT <T>
+    where T: Copy + AddAssign + Sub<Output = T>
+    {
+        pub fn new(size: usize, init: T) -> Self {
+            let bit_size = size.next_power_of_two();
+            BIT {
+                bits: vec![init; bit_size + 1],
+                init: init,
+            }
         }
-        ret
-    }
 
-    // i: 1-indexed
-    pub fn add(&mut self, i: usize, value: u64) {
-        assert!(i > 0);
-        let n = self.bits.len() as i64;
-        let mut i = i as i64;
-        while i <= n {
-            self.bits[i as usize] += value;
-            i += i & -i; // Add a last bit with 1
+        // [l, r] l,r: 1-indexed
+        pub fn range_sum(&self, l: usize, r: usize) -> T {
+            self.sum(r) - self.sum(l-1)
+        }
+
+        // i: 1-indexed but returns 0 if i=0 is given.
+        pub fn sum(&self, i: usize) -> T {
+            let mut i = i as i64;
+            let mut ret = self.init;
+            while i > 0 {
+                ret += self.bits[i as usize];
+                i -= i & -i;
+            }
+            ret
+        }
+
+        // i: 1-indexed
+        pub fn add(&mut self, i: usize, value: T) {
+            assert!(i > 0);
+            let n = self.bits.len() as i64;
+            let mut i = i as i64;
+            while i <= n {
+                self.bits[i as usize] += value;
+                i += i & -i; // Add a last bit with 1
+            }
         }
     }
 }
@@ -42,11 +51,13 @@ impl BIT {
 #[test]
 fn test() {
     let a = [1, 2, 3, 4, 5, 6];
-    let mut bit = BIT::new(a.len());
+    let mut bit = bit::BIT::new(a.len(), 0);
     for i in 0..a.len() {
         bit.add(i + 1, a[i]);
     }
     assert_eq!(bit.sum(1), 1);
     assert_eq!(bit.sum(3), 6);
     assert_eq!(bit.sum(6), 21);
+
+    assert_eq!(bit.range_sum(3, 6), 18);
 }
