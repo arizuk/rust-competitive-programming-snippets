6f37724982b134cae1fed600dd79970836226458
masaki.arizuka <masaki.arizuka@gmail.com>
2019-06-14 01:57:58 +0900

    Fix a bug where raise runtime error

--- src/bit.rs
+++ src/bit.rs
@@ -39,7 +39,7 @@ pub mod ds {
             assert!(i > 0);
             let n = self.data.len() as i64;
             let mut i = i as i64;
-            while i <= n {
+            while i <= n - 1 {
                 self.data[i as usize] += value;
                 i += i & -i; // Add a last bit with 1
             }
@@ -59,4 +59,41 @@ fn test() {
     assert_eq!(bit.sum(6), 21);
 
     assert_eq!(bit.range_sum(3, 7), 18);
+}
+
+#[test]
+fn test_size() {
+    for size in 1..20 {
+        let mut bit = ds::BIT::new(size);
+        let mut ans = 0;
+        for i in 1..size+1 {
+            bit.add(i, 1);
+            ans += 1;
+            assert_eq!(bit.sum(i), ans);
+        }
+    }
+}
+
+#[test]
+fn test_random() {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    const SIZE: usize = 100;
+    let mut bit = ds::BIT::new(SIZE);
+    let mut data = vec![0; SIZE];
+
+    for _ in 0..1000 {
+        let i = rng.gen_range(0, SIZE);
+        let v = rng.gen_range(0, 50);
+        bit.add(i+1, v);
+        data[i] += v;
+
+        let l = rng.gen_range(0, SIZE);
+        let r = rng.gen_range(l, SIZE) + 1;
+
+        let q1 = bit.range_sum(l+1, r+1);
+        let q2 = *&data[l..r].iter().sum();
+        assert_eq!(q1, q2);
+    }
 }
