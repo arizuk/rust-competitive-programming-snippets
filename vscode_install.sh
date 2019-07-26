932b394ed8c470396e7fde0866b37c01b94741e6
masaki.arizuka <masaki.arizuka@gmail.com>
2019-07-26 21:34:11 +0900

    Update snippets

--- vscode_install.sh
+++ vscode_install.sh
@@ -3,5 +3,5 @@ set -eu
 mkdir -p tmp
 cargo snippet -t vscode > tmp/cargo_snippets.json
 jq -s -M add vscode.json tmp/cargo_snippets.json > tmp/rust.json
-echo UPDATE ${VSCODE_SNIPPET_PATH}/rust.json
-cp tmp/rust.json ${VSCODE_SNIPPET_PATH}/rust.json
+echo UPDATE "${VSCODE_SNIPPET_PATH}"/rust.json
+cp tmp/rust.json "${VSCODE_SNIPPET_PATH}"/rust.json
