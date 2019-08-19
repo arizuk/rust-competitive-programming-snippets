5419decc9d2fe49678da7268c40b752a2ee201fd
masaki.arizuka <masaki.arizuka@gmail.com>
2019-08-19 22:39:40 +0900

    Update sliding_window lib

--- src/sliding_window.rs
+++ src/sliding_window.rs
@@ -1,9 +1,11 @@
 #[snippet = "sliding_window"]
 pub mod ds {
     use std::collections::VecDeque;
+
     pub struct SlidingWindowQ<F> {
-        q: VecDeque<usize>,
+        n: usize,
         window: usize,
+        q: VecDeque<usize>,
         cur: usize,
         f: F,
     }
@@ -12,8 +14,21 @@ pub mod ds {
     where
         F: Fn(usize, usize) -> bool,
     {
-        pub fn new(window: usize, f: F) -> Self {
+        /// # Example
+        ///
+        /// ```
+        /// use snippets::sliding_window::ds::SlidingWindowQ;
+        /// let data = vec![2, 3, 1];
+        /// let f = |a, b| data[a] <= data[b];
+        /// let mut q = SlidingWindowQ::new(3, 2, f);
+        /// assert_eq!(q.next(), Some(0));
+        /// assert_eq!(q.next(), Some(0));
+        /// assert_eq!(q.next(), Some(2));
+        /// ```
+        ///
+        pub fn new(n: usize, window: usize, f: F) -> Self {
             SlidingWindowQ {
+                n: n,
                 q: VecDeque::new(),
                 window: window,
                 f: f,
@@ -21,8 +36,11 @@ pub mod ds {
             }
         }
 
-        pub fn next(&mut self) -> usize {
+        pub fn next(&mut self) -> Option<usize> {
             let i = self.cur;
+            if i == self.n {
+                return None
+            }
 
             while self.q.len() > 0 {
                 let j = *self.q.back().unwrap();
@@ -44,45 +62,50 @@ pub mod ds {
             self.front()
         }
 
-        pub fn front(&self) -> usize {
-            *self.q.front().unwrap()
+        pub fn front(&self) -> Option<usize> {
+            self.q.front().map(|&v| v)
         }
     }
 }
 
-#[test]
-fn test_sliding_window_minimum() {
-    let data = [5, 3, 2, 1, 4, 4, 4];
-    let f = |a, b| data[a] <= data[b];
-    let mut q = ds::SlidingWindowQ::new(3, f);
+#[cfg(test)]
+mod tests {
+    use super::ds::SlidingWindowQ;
 
-    let mut ret = vec![];
-    for _ in 0..data.len() {
-        ret.push(q.next());
-    }
-    assert_eq!(ret, vec![0, 1, 2, 3, 3, 3, 6]);
-}
+    #[test]
+    fn test_sliding_window_minimum() {
+        let data = [5, 3, 2, 1, 4, 4, 4];
+        let f = |a, b| data[a] <= data[b];
+        let mut q = SlidingWindowQ::new(data.len(), 3, f);
 
-#[test]
-fn test_sliding_window_minimum_random() {
-    use rand::Rng;
-    let mut rng = rand::thread_rng();
-
-    for _ in 0..10 {
-        let data_size = rng.gen_range(1, 1000);
-        let mut data = vec![];
-        for _ in 0..data_size {
-            data.push(rng.gen_range(0, 1000));
+        let mut ret = vec![];
+        while let Some(idx) = q.next() {
+            ret.push(idx);
         }
-        let window = rng.gen_range(1, data_size + 1);
-        let mut q = ds::SlidingWindowQ::new(window, |a, b| data[a] <= data[b]);
+        assert_eq!(ret, vec![0, 1, 2, 3, 3, 3, 6]);
+    }
 
-        for i in 0..data_size {
-            let idx = q.next();
-            let l = if i + 1 >= window { i + 1 - window } else { 0 };
-            let r = i + 1;
-            let expected = *(&data[l..r]).iter().min().unwrap();
-            assert_eq!(data[idx], expected);
+    #[test]
+    fn test_sliding_window_minimum_random() {
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..10 {
+            let data_size = rng.gen_range(1, 1000);
+            let mut data = vec![];
+            for _ in 0..data_size {
+                data.push(rng.gen_range(0, 1000));
+            }
+            let window = rng.gen_range(1, data_size + 1);
+            let mut q = SlidingWindowQ::new(data_size, window, |a, b| data[a] <= data[b]);
+
+            for i in 0..data_size {
+                let idx = q.next().unwrap();
+                let l = if i + 1 >= window { i + 1 - window } else { 0 };
+                let r = i + 1;
+                let expected = *(&data[l..r]).iter().min().unwrap();
+                assert_eq!(data[idx], expected);
+            }
         }
     }
 }
